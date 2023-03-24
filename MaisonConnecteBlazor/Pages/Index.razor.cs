53b6dafa40842017e4ea21179fe2a6dac10778b8
ThuGapy <61449423+ThuGapy@users.noreply.github.com>
2023-03-24 15:43:10 -0400

    Initial Commit

---
+++ MaisonConnecteBlazor/Pages/Index.razor.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.VisualStudio.Threading;
+using SimpleTCP;
+using System.Diagnostics;
+
+namespace MaisonConnecteBlazor.Pages
+{
+    public partial class Index : ComponentBase
+    {
+        private SimpleTcpClient socketClient = new SimpleTcpClient();
+        public string Image64 { get; set; } = string.Empty;
+
+        protected override Task OnInitializedAsync()
+        {
+            Task.Run(SetupSocketConnection).Forget();
+
+            return base.OnInitializedAsync();
+        }
+
+        public Task SetupSocketConnection()
+        {
+            socketClient.Connect("10.10.211.27", 8010);
+            socketClient.StringEncoder = System.Text.Encoding.ASCII;
+            socketClient.DataReceived += (sender, msg) =>
+            {
+                Debug.WriteLine(msg.MessageString);
+                Image64 = "data:image/png; base64, " + msg.MessageString;
+                UpdateImage();
+            };
+
+            return Task.CompletedTask;
+        }
+          
+        public async void UpdateImage()
+        {
+            await InvokeAsync(StateHasChanged);
+        }
+    }
+}
